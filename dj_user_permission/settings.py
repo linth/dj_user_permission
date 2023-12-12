8c63f0ca4de46b5f11d632a46db988b49f26a43e
gerogelin <gerogelin@oringnet.com>
2023-12-12 10:14:04 +0800

    feat: add generate faker data application.

--- dj_user_permission/settings.py
+++ dj_user_permission/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'custom_users',
+    'gen_faker_data',
 ]
 
 MIDDLEWARE = [
---
+++ gen_faker_data/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GenFakerDataConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gen_faker_data'
